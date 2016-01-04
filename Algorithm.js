59426f4246189783ff24ab4d016e2d9078782798
TempRes <rescollection@126.com>
2016-01-04 23:51:46 +0800

    Update Algorithm.js

--- Algorithm.js
+++ Algorithm.js
@@ -4,10 +4,20 @@ var INF = 123456789;
 function Dijkstra(source, target, adjMatrix, indexMatrix, n){
 	var totalDist = 0;
 	var edgeSet = new Array();
+	for (var i = 0; i < n; i++){
+		edgeSet[i] = new Array();
+	}
+	//最短距离数组
+	var dist = new Array();
+	for (var i = 0; i < n; i++){
+		dist[i] = INF;
+	}
+	//异常处理
 	if ((source == -1)||(target == -1)){
 		return {
-			edgeSet:edgeSet,
-			totalDist:totalDist
+			edgeSet: edgeSet,
+			totalDist: dist[target],
+			disArray: dist
 		}
 	}
 	//创建点集合向量
@@ -22,11 +32,6 @@ function Dijkstra(source, target, adjMatrix, indexMatrix, n){
 		prev[i] = source;
 	}
 	var mainNode = source;
-	//最短距离数组
-	var dist = new Array();
-	for (var i = 0; i < n; i++){
-		dist[i] = INF;
-	}
 	dist[source] = 0;
 	//寻找最短路径
 	while(1){
@@ -54,16 +59,22 @@ function Dijkstra(source, target, adjMatrix, indexMatrix, n){
 		nodeused[next] = 1;
 		mainNode = next;
 	}
-	//整理路径
-	var temp = target;
-	while (temp != source){
-		edgeSet.push(indexMatrix[prev[temp]][temp]);
-		temp = prev[temp];
+
+	//整理所有路径
+	for (var i = 0; i < n; i++){
+		var temp = i;
+		while (temp != source){
+			if (indexMatrix[prev[temp]][temp] == -1) break;
+			edgeSet[i].push(indexMatrix[prev[temp]][temp]);
+			temp = prev[temp];
+		}
 	}
-	
+
 	return {
-		edgeSet: edgeSet,
-		totalDist: dist[target]
+		edgeSet: edgeSet[target],
+		totalDist: dist[target],
+		disArray: dist,
+		AllPaths: edgeSet
 	}
 }
 
@@ -112,5 +123,48 @@ function Prim(root, adjMatrix, indexMatrix, n){
 	return {
 		edgeSet:edgeSet,
 		totalDist:totalDist
+	}
+}
+
+//介数中心度
+function BetweenCentrality(edges, adjMatrix, indexMatrix, n){
+	var countArray = new Array();
+	for (var i = 0; i < n; i++){
+		countArray[i] = 0;
+	}
+	
+	//遍历所有边并计数
+	for (var i = 0; i < n; i++){
+		var result = Dijkstra(i, 50, adjMatrix, indexMatrix, n);
+		for (var j = 0; j < n; j++){
+			if (result.AllPaths[j].length == 0) continue;
+			for (var k = 0; k < result.AllPaths[j].length; k++){
+				countArray[edges[result.AllPaths[j][k]]['target'].index]++;
+				countArray[edges[result.AllPaths[j][k]]['source'].index]++;
+			}
+		}
+	}
+	
+	return {
+		betweenArray: countArray
+	}
+}
+
+//紧密中心度
+function CloseCentrality(adjMatrix, indexMatrix, n){
+	var sumArray = new Array();
+	//遍历所有边并求和
+	for (var i = 0; i < n; i++){
+		var result = Dijkstra(i, 50, adjMatrix, indexMatrix, n);
+		sumArray[i] = 0;
+		for (var j = 0; j < n; j++){
+			if (result.disArray[j] >= INF)
+				continue;
+			sumArray[i] += result.disArray[j];
+		}
+	}
+	
+	return {
+		closeArray: sumArray
 	}
 }
